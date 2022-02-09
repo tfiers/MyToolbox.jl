2d3f7ac284e4128d501fcdc8790a087320ab0706
Tomas Fiers <tomas.fiers@gmail.com>
2022-02-09 03:25:26 +0000

    ziplongest: maxlen, not N

--- src/misctools.jl
+++ src/misctools.jl
@@ -2,7 +2,7 @@
 # `zip_longest` is not in Base.Itertools, and not merged yet in IterTools.jl.
 # Hence, this.
 function ziplongest(iters...; sentinel = nothing)
-    N = maximum(length, iters)  # only iterators with a `length` are supported
-    fill_out(iter) = Iterators.flatten([iter, repeated(sentinel, N - length(iter))])
+    maxlen = maximum(length, iters)  # only iterators with a `length` are supported
+    fill_out(iter) = Iterators.flatten([iter, repeated(sentinel, maxlen - length(iter))])
     return zip(fill_out.(iters)...)
 end
