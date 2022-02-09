c9690c7f7bfc2fc9834a1d77cf2893de3730e533
Tomas Fiers <tomas.fiers@gmail.com>
2022-02-09 03:24:15 +0000

    simplify ziplongest (check implicit, and in comment)

--- src/misctools.jl
+++ src/misctools.jl
@@ -2,9 +2,7 @@
 # `zip_longest` is not in Base.Itertools, and not merged yet in IterTools.jl.
 # Hence, this.
 function ziplongest(iters...; sentinel = nothing)
-    all(iter -> Base.IteratorSize(typeof(iter)) == Base.HasLength(), iters) ||
-        error("only iterators with a `length` are supported")
-    N = maximum(length, iters)
+    N = maximum(length, iters)  # only iterators with a `length` are supported
     fill_out(iter) = Iterators.flatten([iter, repeated(sentinel, N - length(iter))])
     return zip(fill_out.(iters)...)
 end
