86c888bff37c6aa2658bfce1f6cae852cad83324
Tomas Fiers <tomas.fiers@gmail.com>
2022-03-11 09:25:45 +0000

    reorganize imports-exports (cleaner)

--- src/MyToolbox.jl
+++ src/MyToolbox.jl
@@ -1,29 +1,35 @@
 module MyToolbox
 
+
 using Reexport
 export @reexport
 
-@reexport using Requires
-@reexport using Base.Iterators         # `partition`, `cycle`, `flatten` ("chain"), …
-@reexport using DataStructures         # `DefaultDict`, `OrderedDict`, `counter`, queues, …
-@reexport using Random
-@reexport using StatsBase              # `sample`, `describe`, …. Plus: reexports `Statistics` from stdlib
-@reexport using PartialFunctions,      # Currying (`func $ a`, like `partial(func, a)` in Python)
-                PyFormattedStrings,    # f-strings as in Python (but with C format spec)
-                LaTeXStrings,          # `L"These strings can contain $ and \ without escaping"`
-                FilePaths,             # `Path` type and `/` joins, as in Python
-                Printf                 # `@printf`, `@sprintf`
-@reexport using IJulia
-@reexport using ProgressMeter          # `@showprogress`
-@reexport using BenchmarkTools: @benchmark, @btime  # Don't import all, bc that includes `params`,
-                                                    # which conflicts when `Distributions` is loaded.
-@reexport using Profile, ProfileView   # `@profview`
-@reexport using Logging, LoggingExtras
-@reexport using ComponentArrays
-@reexport using Setfield: @set, @set!  # `immut = @set immut.some.field = new`.
-                                       # Method (not macro) `set` clashes with Sciplotlib's.
-@reexport using Parameters             # `@unpack`, `@pack!`, `@with_kw` (> `Base.@kwdef`)
-@reexport using Pkg
+@reexport using Pkg,
+    Random,
+    Printf,
+    Logging,
+    Profile,
+    Base.Iterators,        # `partition`, `cycle`, `flatten` (= chain), …
+
+    DataStructures,        # `DefaultDict`, `OrderedDict`, `counter`, queues, …
+    StatsBase,             # `sample`, `describe`, …. Plus: reexports `Statistics` from stdlib
+    PartialFunctions,      # Currying (`func $ a`, like `partial(func, a)` in Python)
+    PyFormattedStrings,    # f-strings as in Python (but with C format spec)
+    LaTeXStrings,          # `L"These strings can contain $ and \ without escaping"`
+    FilePaths,             # `Path` type and `/` joins, as in Python
+    Requires,              # `@require PkgName = "…" …` in `__init__`.
+    IJulia,
+    ProgressMeter,         # `@showprogress`
+    BenchmarkTools: @benchmark, @btime,
+                           # BenchmarkTools exports `params`, which conflicts with
+                           # `Distributions` when that is loaded.
+    ProfileView,           # `@profview`
+    LoggingExtras,         # `TeeLogger`, `ActiveFilteredLogger`, …
+    ComponentArrays,
+    Setfield: @set, @set!, # `immut = @set immut.some.field = new`.
+                           # `set` (the method not macro) clashes with Sciplotlib's.
+    Parameters             # `@unpack`, `@pack!`, `@with_kw` (> `Base.@kwdef`)
+
 
 include("macros.jl")
 export @alias
@@ -40,9 +46,11 @@ export ziplongest, chain
 include("misctools.jl")
 export resetrng!, linspace
 
+
 function __init__()
-    # See includes below for additional imports and @reexports of MyToolbox, besides those
-    # in the current file.
+
+    # See included files below for additional imports and @reexports of MyToolbox, besides
+    # those in this file.
     @require PyPlot = "d330b81b-6aea-500a-939a-2ce795aea3ee" begin
         include("figures.jl")
         export savefig
@@ -51,10 +59,12 @@ function __init__()
         include("dataframes.jl")
         export printsimple
     end
+
     if isdefined(Main, :IJulia) && Main.IJulia.inited
         prettify_logging_in_IJulia()
     end
-    # [a todo: eval this in Main: `using FilePathsBase: /`]
+    # [a todo: eval `using FilePathsBase: /` in Main]
 end
+
 
 end # module
