fd228d266fefcbe870d7cfa36b89039dda97fabe
Tomas Fiers <tomas.fiers@gmail.com>
2022-03-11 08:43:05 +0000

    ok no it did work, problem was just submodule: does not inheirt parent imports

--- src/MyToolbox.jl
+++ src/MyToolbox.jl
@@ -1,9 +1,6 @@
 module MyToolbox
 
 using Reexport
-using Requires
-# `@reexport using Requires` and then `using MyToolbox; @require …` in a module does not
-# seem to work.
 
 @reexport using Requires
 @reexport using Base.Iterators         # `partition`, `cycle`, `flatten` ("chain"), …
