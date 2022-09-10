37031b5a84b8ba4d403afaf2f0f73289017e58af
Tomas Fiers <tomas.fiers@gmail.com>
2022-09-10 03:19:46 +0200

    df printsimple: make defaults customizable

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -3,12 +3,12 @@
 
 # See https://dataframes.juliadata.org/stable/lib/functions/#Base.show
 # and https://ronisbr.github.io/PrettyTables.jl/stable/man/usage/
-printsimple(df::DataFrame; kw...) = show(
-    df;
-    summary = false,
-    eltypes = false,
+printsimple(
+    df::DataFrame;
+    summary         = false,
+    eltypes         = false,
     show_row_number = false,
-    formatters = ft_printf("%.3g"),
-    alignment = :l,
+    formatters      = ft_printf("%.3g"),
+    alignment       = :l,                      # for text, not numbers
     kw...
-)
+) = show(df; summary, eltypes, show_row_number, formatters, alignment, kw...)
